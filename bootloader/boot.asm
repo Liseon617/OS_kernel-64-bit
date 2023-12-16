5e7ccb39e681b09054b110d7b6344d9c6390b00b
Liseon617 <64420980+Liseon617@users.noreply.github.com>
2023-12-17 02:42:45 +0800

    print alphabet
    alternating letters

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -1,14 +1,26 @@
 mov ah, 0x0e
-mov al, 65
+mov dx, 25
+mov cx, 0
+mov al, 65 + 32; print 'a'
 int 0x10
 
-mov ah, 0x0e
-mov al, 0x41
-int 0x10
+loop: 
+    inc al
+    cmp cx, dx
+    je $
+    sub al, 32
+    int 0x10
+    inc cx
 
-mov ah, 0x0e
-mov al, 0b01000001
-int 0x10
+    inc al
+    cmp cx, dx
+    je $
+    add al, 32
+    int 0x10
+    inc cx
+
+    jmp loop ;loop
+
 
 jmp $ ;ef fd ff
 
